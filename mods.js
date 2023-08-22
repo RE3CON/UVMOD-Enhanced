78a504a2d57c1cc5df3a00a0078e467dccc15c3b
Chris <write2recon@gmail.com>
2023-08-22 16:45:44 +0200

    Update mods.js

--- mods.js
+++ mods.js
@@ -138,7 +138,52 @@ class Mod_changeTone extends FirmwareMod {
             return firmwareData;
         }
     }
-    ,  //add 500k steps @0xe0d2: 0xC4 0x09 changed to 0x50 0xC3   
+    ,  //add 500k steps @0xe0d2: 0xC4 0x09 changed to 0x50 0xC3  
+   class Mod_ChangeRXLimits extends FirmwareMod {
+        constructor() {
+            super("Extend RX Limits - Eperimental", "Allows receive in the specified frequency range.", 0);
+            this.inputMinTX = addInputField(this.modSpecificDiv, "Specify a new value for the minimum frequency in the range 18-1300 MHz:", "50");
+            this.inputMaxTX = addInputField(this.modSpecificDiv, "Specify a new value for the minimum frequency in the range 18-1300 MHz:", "600");
+         //   this.selectRX = addRadioButton(this.modSpecificDiv, "RX", "selectSbar", "selectApp");
+         //   this.selectRXTX = addRadioButton(this.modSpecificDiv, "RX + TX", "selectSbar", "selectApp");
+            //this.hidden = true;
+            
+        }
+
+        apply(firmwareData) {
+            
+            const offsetlow = 0xe078;
+            const offsethi = 0xe0a8;
+            const txStart = parseInt(this.inputMinTX.value) * 100000;
+            const txStop = parseInt(this.inputMaxTX.value) * 100000;
+
+            if ((txStart <= txStop) && (txStart >= 100000) && (txStart <= 130000000) && (txStop >= 100000) && (txStop <= 130000000)) {
+
+                const buffer = new ArrayBuffer(8);
+                const dataView = new DataView(buffer);
+
+                dataView.setUint32(0, txStart, true);
+                
+
+                const txHex = new Uint8Array(buffer);
+
+                firmwareData = replaceSection(firmwareData, txHex, offsetlow);
+                dataView.setUint32(0, txStop, true);
+                firmwareData = replaceSection(firmwareData, txHex, offsethi);
+           //     if (this.selectRX.checked) {}
+            //    else if (this.selectRXTX.checked) {
+              //  firmwareData = replaceSection(firmwareData, hexString("1868"), 0x180e);}//43CC
+                //else
+                log(`Success: ${this.name} applied.`);
+            }
+            else {
+                log(`Error in ${this.name}: Incorrect data! The frequencies must be greater than 18 MHz and less than 1300 MHz, the maximum greater than or equal to the minimum. `);
+            }
+
+            return firmwareData;
+        }
+    }
+    ,            
     class Mod_ChangeTXLimits extends FirmwareMod {  // there's something, a lot wrong... and meed more coding skills. It must patch for TX and RX 3 or 4 longer hex strings on different locations/offsets!
         constructor() {
             super("Extend TX Limits (Experimental)", "Allows transmission on the specified frequency range.", 0);
